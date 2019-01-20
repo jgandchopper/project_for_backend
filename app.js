f60625b05c2813650479cc0c2555ab811df3f9d1
cjg9040 <chl8273@kookmin.ac.kr>
2019-01-20 22:22:24 +0900

    settingfin

--- app.js
+++ app.js
@@ -53,3 +53,6 @@ app.use((err,req,res)=>{
 app.listen(app.get('port'),()=>{
     console.log(app.get('port'),'번 포트에서 대기 중');
 });
+
+
+
--- models/index.js
+++ models/index.js
@@ -11,23 +11,10 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 db.User = require('./user')(sequelize,Sequelize);
 db.Post = require('./post')(sequelize,Sequelize);
-db.Hashtag = require('./hashtag')(sequelize,Sequelize);
 db.User.hasMany(db.Post);
-db.Post.belongsTo(db.User);
-db.Post.belongsToMany(db.Hashtag,{through:'PostHashtag'});
-db.Hashtag.belongsToMany(db.Post,{through:'PostHashtag'});
 
 
-//유저간 팔로워도 N:M 관계이다.
-db.User.belongsToMany(db.User,{
-  foreignKey:'followingId',
-  as:'Followers', // join 작업시 사용하는 이름.
-  through:'Follow'//모델 이름.
-});
-db.User.belongsToMany(db.User,{
-  foreignKey:'followerId',
-  as:'Following',
-  through:'Follow'
-});
+
+
 
 module.exports = db;
