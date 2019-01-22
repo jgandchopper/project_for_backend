77a210c7d7fbb750260a455b15ffda4b0be7f958
ChanHO <hpyho33@kookmin.ac.kr>
2019-01-22 20:50:46 +0900

    file read

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.json')[env];
 const db = {};
+const fs = require('fs');
 
 const sequelize= new Sequelize(
   config.database,config.username,config.password,config
@@ -9,11 +10,22 @@ const sequelize= new Sequelize(
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
+fs.readdirSync(__dirname)
+    .filter(function(file) {
+        return (file.indexOf('.') !== 0) && (file !== 'index.js');
+    })
+    .forEach(function(file) {
+        var model = sequelize['import'](path.join(__dirname, file));
+        db[model.name] = model;
+    });
+
+/*
 db.User = require('./user')(sequelize,Sequelize);
 db.Post = require('./post')(sequelize,Sequelize);
 db.User.hasMany(db.Post);
 db.Item = require('./item')(sequelize,Sequelize);
-
+*/
 
 
 
